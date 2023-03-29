2a12085d094259978e8bf4d8f340a316549f4300
maximilionus <maximilionuss@gmail.com>
2023-03-30 02:52:04 +0300

    Full re-build using the `chota` framework

--- js/main.js
+++ js/main.js
@@ -1,21 +1,15 @@
-function validateEmail (email) {
-    return email.match(
-        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-}
-
 function call_keygen() {
     let input_email = document.getElementById('input_email').value
-    let vs_code_mode = document.querySelector('input[id="radio_mode_vscode"]:checked');
-    let sublime_code_mode = document.querySelector('input[id="radio_mode_sublime"]:checked');
+    let vscode_mode = document.querySelector('input[id="radio_mode_vscode"]:checked');
+    let sublime_mode = document.querySelector('input[id="radio_mode_sublime"]:checked');
 
     if (validateEmail(input_email) === null) {
         return false;
     };
 
-    if (vs_code_mode !== null) {
+    if (vscode_mode !== null) {
         display_output(keygen_vscode(input_email));
-    } else if (sublime_code_mode !== null) {
+    } else if (sublime_mode !== null) {
         display_output(keygen_sublime(input_email));
     }
 }
@@ -25,7 +19,7 @@ function display_output(text_to_display) {
     let text_field = document.getElementsByClassName("result_output_text")[0];
 
     text_field.innerHTML = text_to_display;
-    output_field.style.opacity = 100;
+    output_field.style.opacity = 1;
 }
 
 function input_field_keypress_handler(event) {
@@ -33,13 +27,16 @@ function input_field_keypress_handler(event) {
         call_keygen();
     } else {
         // Fix the visual response to email regex match
-        let email_field = document.querySelector('#email_input_field');
-        let email_input_field = email_field.children.input_email;
+        let input_container = document.querySelector('#input_container');
+        let email_input_field = input_container.children.input_email;
+        let button_generate = document.querySelector('#btn_generate');
 
         if (validateEmail(email_input_field.value) != null) {
-            email_input_field.setCustomValidity('');
+            email_input_field.className = 'success';
+            button_generate.disabled = false;
         } else {
-            email_input_field.setCustomValidity('wrong email');
+            email_input_field.className = 'error';
+            button_generate.disabled = true;
         }
     }
 }
