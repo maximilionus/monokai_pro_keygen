de0c586737283d3205f6a5f4b0a12ac31b36941e
maximilionus <maximilionuss@gmail.com>
2023-03-30 04:03:52 +0300

    Add key copy btn svg, enhance formatting

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function call_keygen() {
 }
 
 function display_output(text_to_display) {
-    let output_field = document.getElementsByClassName("result_output_field")[0];
+    let output_field = document.getElementsByClassName("result_output_box")[0];
     let text_field = document.getElementsByClassName("result_output_text")[0];
 
     text_field.innerHTML = text_to_display;
