049f137fac43324d2d08dd7b44f10970e9de7031
maximilionus <maximilionuss@gmail.com>
2023-03-30 04:38:24 +0300

    Done copy to clipboard, enhance core.js, adapt

--- js/core.js
+++ js/core.js
@@ -1,5 +1,7 @@
 function validateEmail (email) {
-    return email.match(
+    result = email.match(
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
+
+    return (result == null ? false : true)
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+// Global vars
+var generated_key = '';
+
+
 function call_keygen() {
-    let input_email = document.getElementById('input_email').value
+    let input_email = document.getElementById('input_email').value;
     let vscode_mode = document.querySelector('input[id="radio_mode_vscode"]:checked');
     let sublime_mode = document.querySelector('input[id="radio_mode_sublime"]:checked');
 
@@ -8,10 +12,12 @@ function call_keygen() {
     };
 
     if (vscode_mode !== null) {
-        display_output(keygen_vscode(input_email));
+        generated_key = keygen_vscode(input_email);
     } else if (sublime_mode !== null) {
-        display_output(keygen_sublime(input_email));
+        generated_key = keygen_sublime(input_email);
     }
+
+    display_output(generated_key);
 }
 
 function display_output(text_to_display) {
@@ -22,16 +28,26 @@ function display_output(text_to_display) {
     output_field.style.opacity = 1;
 }
 
+function copy_key_to_clipboard() {
+    // TODO: Add fallback for unsupported engines
+    navigator.clipboard.writeText(generated_key);
+}
+
 function input_field_keypress_handler(event) {
     if (event.key === "Enter") {
         call_keygen();
     } else {
-        // Fix the visual response to email regex match
         let input_container = document.querySelector('#input_container');
         let email_input_field = input_container.children.input_email;
         let button_generate = document.querySelector('#btn_generate');
 
-        if (validateEmail(email_input_field.value) != null) {
+        if (email_input_field.value.length == 0) {
+            email_input_field.className = '';
+            button_generate.disabled = true;
+            return;
+        }
+
+        if (validateEmail(email_input_field.value) == true) {
             email_input_field.className = 'success';
             button_generate.disabled = false;
         } else {
